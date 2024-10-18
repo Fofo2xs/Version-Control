f4f4a5db08f9cf8bac42fe83909b6caa6a315ce6
Sephora <spierrelouis@ufl.edu>
2024-10-17 23:38:25 -0400

    "Added main function and encoder -- Sephora"

---
+++ version_control.py
@@ -0,0 +1,47 @@
+
+#Sephora Pierre-Louis
+
+def encode(password):
+    password_list = list(password)
+    encoded_list = []
+    for item in password_list:
+        item = int(item)
+        item += 3
+        encoded_list.append(str(item))
+
+    encoded_string = ''.join(encoded_list)
+
+    return encoded_string
+
+
+def decode(password):
+    pass
+
+
+if __name__ == '__main__':
+
+    while True:
+
+        print()
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        print()
+
+        option =  int(input("Please enter an option: "))
+
+        if option == 1:
+            password = input("Please enter a password to encode: ")
+            print("Your password has been encoded and stored!")
+            password_encoded = encode(password)
+
+        elif option == 2:
+            print(f"The encoded password is {password_encoded}, and the original password is {decode(password_encoded)}.")
+
+        elif option == 3:
+            break
+
+
+
